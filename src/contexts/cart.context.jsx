1749241914d8912a3889042752c90a6927214b16
Jason Hartsell (Astoundcommerce) <j.hartsell@astoundcommerce.com>
2023-05-01 12:54:46 -0700

    Refactor contexts

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
@@ -42,32 +43,73 @@ export const CartContext = createContext({
     cartTotal: 0
 });
 
+const CART_ACTION_TYPES = {
+    SET_CART_OPEN: 'SET_CART_OPEN',
+    SET_CART_ITEMS: 'SET_CART_ITEMS'
+};
+
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case CART_ACTION_TYPES.SET_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen: payload
+            };
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            };
+        default:
+            throw new Error(`Unhandle type ${type} in cartReducer`);
+    }
+};
+
 export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal, setCartTotal] = useState(0);
+    const [{isCartOpen, cartItems, cartCount,  cartTotal}, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
-        setCartCount(newCartCount);
-    }, [cartItems]);
+    const updateCartItemsReducer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        const newCartTotal = newCartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
 
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
-        setCartTotal(newCartTotal);
-    }, [cartItems]);
+        dispatch(
+            createAction(
+                CART_ACTION_TYPES.SET_CART_ITEMS, 
+                {
+                    cartItems: newCartItems,
+                    cartTotal: newCartTotal,
+                    cartCount: newCartCount
+                }
+            )
+        );
+    };
+
+    const setIsCartOpen = (isOpen) => {
+        dispatch(createAction(CART_ACTION_TYPES.SET_CART_OPEN, isOpen));
+    };
 
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd));
+        const newCartItems = addCartItem(cartItems, productToAdd);
+        updateCartItemsReducer(newCartItems);
     };
 
     const removeItemFromCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     };
 
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems, cartItemToClear));
+        const newCartItems = clearCartItem(cartItems, cartItemToClear);
+        updateCartItemsReducer(newCartItems);
     };
 
     const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartCount, cartTotal };
--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
@@ -6,8 +8,35 @@ export const CategoriesContext = createContext({
     categoriesMap: {},
 });
 
+const CATEGORY_ACTION_TYPES = {
+    SET_CATEGORY_MAP: 'SET_CATEGORY_MAP'
+};
+
+const categoriesReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case CATEGORY_ACTION_TYPES.SET_CATEGORY_MAP:
+            return {
+                ...state,
+                categoriesMap: payload
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in categoriesReducer`);
+    }
+};
+
+const INITIAL_STATE = {
+    categoriesMap: {}
+};
+
 export const CategoriesProvider = ({children}) => {
-    const [categoriesMap, setCategoriesMap] = useState({});
+    const [{ categoriesMap }, dispatch ] = useReducer(categoriesReducer, INITIAL_STATE);
+
+    const setCategoriesMap = (categoryMap) => {
+        dispatch(createAction(CATEGORY_ACTION_TYPES.SET_CATEGORY_MAP, categoryMap));
+    };
+
     const value = { categoriesMap };
 
     useEffect(() => {
