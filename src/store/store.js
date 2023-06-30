95ad07365a1643bb6f15188b6cae63c30197fe04
Jason Hartsell (Astoundcommerce) <j.hartsell@astoundcommerce.com>
2023-06-30 13:59:18 -0700

    Add redux thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,13 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+// import { loggerMiddleware } from './middleware/logger';
+import thunk from 'redux-thunk';
 
 // root reducer
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state', store.getState());
-};
 
 const persistConfig = {
     key: 'root',
@@ -30,9 +17,14 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger, 
+    thunk
+].filter(Boolean);
 
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
