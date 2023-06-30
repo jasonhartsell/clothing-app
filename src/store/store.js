fd67e122583eb191968ec62be5b11443f3d3b124
Jason Hartsell (Astoundcommerce) <j.hartsell@astoundcommerce.com>
2023-06-30 15:02:24 -0700

    Migrate from thunk to saga

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,11 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 // import { loggerMiddleware } from './middleware/logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+
+import { rootSaga } from './root-saga';
 
 // root reducer
 
@@ -15,11 +19,13 @@ const persistConfig = {
     blacklist: ['user'],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
     process.env.NODE_ENV !== 'production' && logger, 
-    thunk
+    sagaMiddleware
 ].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
@@ -27,5 +33,7 @@ const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && wind
 const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
