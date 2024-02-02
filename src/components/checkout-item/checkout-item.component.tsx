6234906e6c4711d5567bf051435387397e168cd7
Jason Hartsell <j.hartsell@astoundcommerce.com>
2024-02-02 11:02:25 -0800

    Add performance optimizations

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -25,7 +25,7 @@ type CartItemProps = {
   cartItem: TCartItem;
 };
 
-const CheckoutItem : FC<CartItemProps> = ({ cartItem }) => {
+const CheckoutItem : FC<CartItemProps> = memo(({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   const cartItems = useSelector(selectCartItems);
   const dispatch = useDispatch();
@@ -51,6 +51,6 @@ const CheckoutItem : FC<CartItemProps> = ({ cartItem }) => {
       <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
     </CheckoutItemContainer>
   );
-};
+});
 
 export default CheckoutItem;
